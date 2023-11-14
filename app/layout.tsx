632ba145ea87daba01b3405b607a8a093ae4cf70
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-14 12:59:48 +0200

    fetch data using useEffect

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Nunito_Sans } from 'next/font/google';
-import '.globals.css';
+import './globals.css';
 
 const nunito = Nunito_Sans({ subsets: ['latin']});
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+'use client';
+import { useState, useEffect } from 'react';
+
+const Home = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=name,capital,region,flags,population')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if(isLoading) return <p>Loading...</p>
+
+  return (
+    <img src={data[2].flags.png} alt={data[2].flags.alt} />
+  );
+} 
+
+export default Home;
