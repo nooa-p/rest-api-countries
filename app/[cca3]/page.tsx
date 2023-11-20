128279d47c088c67ffbbfcfa114d9af1523f2687
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-20 13:46:00 +0200

    fix import errors

--- app/[cca3]/page.tsx
+++ app/[cca3]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
-import { Switcher } from "./switcher";
+import { Switcher } from "../switcher";
 // fetching from local file
-import data from "../json/all.json";
+import data from "../../json/all.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import Link from 'next/link';
