7ba976dbb455c5c3229bd4f852b44a07a2966fdc
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-20 13:44:44 +0200

    imports to country page

--- app/[cca3]/page.tsx
+++ app/[cca3]/page.tsx
@@ -1,4 +1,10 @@
 'use client'
+import { Switcher } from "./switcher";
+// fetching from local file
+import data from "../json/all.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Link from 'next/link';
 
 export default function Page({ params }: { params : { cca3: string }}) {
     return <div>Test: {params.cca3}</div>
