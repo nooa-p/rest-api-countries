da9c59badd8426fc4ed2cb5c65ace60de1ce2eed
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-21 13:20:46 +0200

    remove border from focused search bar

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ const Home = () => {
                 id=""
                 placeholder="Search for a country..."
                 onChange={(e) => setSearch(e.target.value)}
-                className="bg-white dark:bg-[#2B3945] p-3.5 pl-20 -ml-[46px] z-0 relative w-full md:w-2/3 rounded md:max-w-[450px] placeholder:text-[#111517] placeholder:dark:text-white shadow"
+                className="bg-white dark:bg-[#2B3945] p-3.5 pl-20 -ml-[46px] z-0 relative w-full md:w-2/3 rounded md:max-w-[450px] placeholder:text-[#111517] placeholder:dark:text-white shadow focus:outline-none"
               />
             </div>
             <select
