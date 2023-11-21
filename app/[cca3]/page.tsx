4bcfebf6e73a71ffef33300447317c76f629ace7
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-21 09:54:59 +0200

    add divs & lists for info

--- app/[cca3]/page.tsx
+++ app/[cca3]/page.tsx
@@ -21,6 +21,33 @@ export default function Page({ params }: { params : { cca3: string }}) {
             <FontAwesomeIcon icon={faArrowLeft} />
             <Link href='/'>Back</Link>
         </div>
+        <div className="flex flex-col md:flex-row">
+            <div className="md:basis-1/2">
+                <img src={filtered[0].flags.svg} alt="" />
+            </div>
+            <div className="md:basis-1/2">
+                <h1>{filtered[0].name.common}</h1>
+                <div className="flex flex-col md:flex-row">
+                    <ul>
+                        <li><span className="font-semibold">Native Name:</span> <span className="font-light"></span></li>
+                        <li><span className="font-semibold">Population:</span> <span className="font-light">{filtered[0].population
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></li>
+                        <li><span className="font-semibold">Region:</span> <span className="font-light">{filtered[0].region}</span></li>
+                        <li><span className="font-semibold">Sub Region:</span> <span className="font-light">{filtered[0].subregion}</span></li>
+                        <li><span className="font-semibold">Capital:</span> <span className="font-light">{filtered[0].capital}</span></li>
+                    </ul>
+                    <ul>
+                    <li><span className="font-semibold">Top Level Domain:</span> <span className="font-light">{filtered[0].tld}</span></li>
+                    <li><span className="font-semibold">Currencies:</span> <span className="font-light"></span></li>
+                    <li><span className="font-semibold">Languages:</span> <span className="font-light"></span></li>
+                    </ul>
+                </div>
+                <div>
+                <span className="font-semibold">Border Countries:</span>
+                </div>
+            </div>
+        </div>
         </main>
         </>
     )
