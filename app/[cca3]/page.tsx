b7c8550802716c21881dec59dd80169f27371789
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-21 09:34:45 +0200

    routing to make border countries possible, added link to front page

--- app/[cca3]/page.tsx
+++ app/[cca3]/page.tsx
@@ -3,9 +3,25 @@ import { Switcher } from "../switcher";
 // fetching from local file
 import data from "../../json/all.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import Link from 'next/link';
 
 export default function Page({ params }: { params : { cca3: string }}) {
-    return <div>Test: {params.cca3}</div>
+    const filtered = data.filter((el) => el.cca3.includes(params.cca3));
+    return (
+        <>
+        <header className="w-full flex bg-white dark:bg-[#2B3945] text-[#111517] dark:text-white p-5 justify-between">
+        <h1 className="font-extrabold text-xl ml-2 md:ml-16">
+          Where in the world?
+        </h1>
+        <Switcher />
+      </header>
+      <main className="mx-2 md:mx-16 p-5 text-sm">
+        <div>
+            <FontAwesomeIcon icon={faArrowLeft} />
+            <Link href='/'>Back</Link>
+        </div>
+        </main>
+        </>
+    )
 }
