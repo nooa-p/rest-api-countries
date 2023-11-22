339d1e9354e0a86c177f5ed77347e606c47a2f4f
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-22 13:03:36 +0200

    try fixing get method compile error

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,5 +1,5 @@
 export async function GET() {
     const res = await fetch('https://restcountries.com/v3.1/all', { next: {revalidate: 3600}})
     const data = await res.json()
-    return data;
+    return new Response(data);
 }
